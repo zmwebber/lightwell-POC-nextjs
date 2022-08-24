84e612d82411743ab459f508039bcec33e3d6495
zac.webber <zac.webber@lightwellinc.com>
2022-08-24 11:20:04 -0400

    products must be wrapped in <Cart>

--- with-stripe-typescript-app/pages/index.tsx
+++ with-stripe-typescript-app/pages/index.tsx
@@ -1,26 +1,25 @@
 import { NextPage } from 'next'
 import Link from 'next/link'
+import Cart from '../components/Cart'
 import Layout from '../components/Layout'
 import Products from '../components/Products'
-
 const IndexPage: NextPage = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>Hello World</h1>
-      <ul className="card-list">        
+      <ul className="card-list">
+      <div><Cart><Products/></Cart></div>        
         <li>
           <Link href="/use-shopping-cart">
             <a className="card cart-style-background">
-              <h2 className="bottom">Use Shopping Cart</h2>
+              <h2 className="bottom">View Cart</h2>
               <img src="/use-shopping-cart.png" />
             </a>
           </Link>
         </li>
       </ul>
-
-      <div>
-        <Products />
-      </div>
+      
+     
     </Layout>
   )
 }
