a03071443f5bcee40f1640389ad7fb04e914ec2c
zac.webber <zac.webber@lightwellinc.com>
2022-08-25 13:22:19 -0400

    making this change. I don't know why Next AppProps are not working with TS

--- with-stripe-typescript-app/pages/_app.tsx
+++ with-stripe-typescript-app/pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/app'
 
 import '../styles.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: any)  {
   return <Component {...pageProps} />
 }
 
